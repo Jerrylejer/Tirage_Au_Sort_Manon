a2ae7330822ce7d75f7a2da7422ce16f16b94689
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-09 21:27:24 +0100

    CONFECTION DU ROUTING POUR LE TIREGA AU SORT + LISTE DES ABSENT.E.S

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbsenceComponent } from './pages/absence/absence.component';
+import { LotteryComponent } from './pages/lottery/lottery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LotteryComponent
+  },
+  {
+    path:"absences",
+    component: AbsenceComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

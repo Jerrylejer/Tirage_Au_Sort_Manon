a9596733616a6d706266f169e82e249e4e73a3a5
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2023-02-14 21:38:36 +0100

    COMMUNICATION ENTRE SERVICE ET COMPONENTS OK - APPRENANT SELECTIONNE EST RENDU DANS LA LSITE DE DROITE RESTE A CREER UNE METHODE DE SUPPRESSION ET FAIRE LA DISTINCTION DE GENRE

--- src/app/components/display-absence-gender-lists/display-absence-gender-lists.component.ts
+++ src/app/components/display-absence-gender-lists/display-absence-gender-lists.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { SharedAbsentsService } from 'src/app/services/shared-absents.service';
-import { AbsenceSelectionListComponent } from '../absence-selection-list/absence-selection-list.component';
-
+import { SharedDatasService } from 'src/app/sharedDatas.service';
 
 @Component({
   selector: 'app-display-absence-gender-lists',
   templateUrl: './display-absence-gender-lists.component.html',
-  styleUrls: ['./display-absence-gender-lists.component.css']
+  styleUrls: ['./display-absence-gender-lists.component.css'],
 })
 export class DisplayAbsenceGenderListsComponent {
+  
+  constructor(public data: SharedDatasService) {}
 
+  ngOnInit() {
+    this.data.absents;
+  }
 
   deleteAbsent() {
 
   }
+
 }
---
+++ src/app/sharedDatas.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedDatasService {
+
+constructor() { }
+// La propriété "absents" peut recevoir une liste pré-enregistrée et/ou la valeur d'un input (voir absence-selection-component.ts et .html)
+absents: string[] = [];
+}
